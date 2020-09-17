835fd16f4c79f8aaa8ab12096e02d229961ba545
amirhsc <amirr10c@gmail.com>
2020-09-17 01:01:11 +0000

    0x00-hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  long long t_llong;
-  float t_float;
-  long t_long;
-  char t_char;
-  int t_int;
+long long t_llong;
+float t_float;
+long t_long;
+char t_char;
+int t_int;
 
-  printf("Size of a char: %lu byte(s)\n", sizeof(t_char));
-  printf("Size of an int: %lu byte(s)\n", sizeof(t_int));
-  printf("Size of a long int: %lu byte(s)\n", sizeof(t_long));
-  printf("Size of a long long int: %lu byte(s)\n", sizeof(t_llong));
-  printf("Size of a float: %lu byte(s)\n", sizeof(t_float));
+printf("Size of a char: %lu byte(s)\n", sizeof(t_char));
+printf("Size of an int: %lu byte(s)\n", sizeof(t_int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(t_long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(t_llong));
+printf("Size of a float: %lu byte(s)\n", sizeof(t_float));
 
-  return (0);
+return (0);
 }
