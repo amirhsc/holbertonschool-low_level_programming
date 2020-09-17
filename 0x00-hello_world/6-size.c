93d7d8675fe6a10320c1c7537ec459fa2b1342b0
amirhsc <amirr10c@gmail.com>
2020-09-17 00:51:04 +0000

    0x00-hello

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
   printf("Size of a long int: %d byte(s)\n", sizeof(long int));
   printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
   printf("Size of a float: %d byte(s)\n", sizeof(float));
+  printf("Size of a short: %d byte(s)\n", sizeof(short));
 
   return (0);
 }
