e7649b8ffe39938512a0f74d30aed8b31036f08d
amirhsc <amirr10c@gmail.com>
2020-09-18 02:47:55 +0000

    0x01 variables

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,23 @@
-#include <stdio.h>
+# include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Write a program that prints the lowercase alphabet
+*  in reverse, followed by a new line.
+* -You can only use the putchar function (every other function
+*  (printf, puts, etc) is forbidden)
+* -All your code should be in the main function
+* -You can only use putchar twice in your code
+* Return: 0
+*/
+
 int main(void)
 {
-char cba;
+int letter;
 
-for (cba = 'z'; cba >= 'a'; cba--)
-	putchar(cba);
-
-putchar('\n');
+for (letter = 122; letter > 96; --letter)
+{
+putchar(letter);
+}
+putchar(10);
 return (0);
 }
- 2020 GitHub, Inc.
